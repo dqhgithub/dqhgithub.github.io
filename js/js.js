0fdc610faacfd6a81fe43156847e1fd75861af84
dengquanhua <718121959@qq.com>
2018-03-26 19:55:55 +0800

    我的作品

--- js/js.js
+++ js/js.js
@@ -1,63 +1,12 @@
 
-/*banner*/
-$(function(){
-				var mySwiper = new Swiper('.swiper1', {
-					autoplay: 5000, //可选选项，自动滑动
-					autoplayDisableOnInteraction: false,
-					pagination: '.swiper-pagination',
-					loop: true,
-					lazyLoading:true,//设为true开启图片延迟加载
-				});
-				var mySwiper = new Swiper('.swiper3', {
-					autoplay: 5000, //可选选项，自动滑动
-					autoplayDisableOnInteraction: false,
-					pagination: '.swiper-pagination',
-					loop: true,
-					lazyLoading: true,
-				});
-				var mySwiper2 = new Swiper('.swiper2', {
-					autoplay: 2500, //可选选项，自动滑动
-					autoplayDisableOnInteraction: false,
-					loop: true,
-					direction: 'vertical'
-				});
-				
-			});
-      var meun_flag = false;
-		$('#meun_icon').click( function() {
-			if(meun_flag) {
-				return false;
-			}
-			var $_this = $(this);
-			if($_this.hasClass('on')) {
-				$_this.removeClass('on')
-				$('#public_meun').removeClass('zoom_big').addClass('zoom_small');
-				$('html,body').removeClass('no_scroll');
-				$('div.black_block').stop().fadeOut();
-				meun_flag = true;
-				var tt = setTimeout(function() {
-					$('#public_meun').hide();
-					$('#public_meun').addClass('zoom_big').removeClass('zoom_small');
-					meun_flag = false;
-					clearTimeout(tt);
-				}, 500)
-			} else {
-				$('#public_meun').show(); 
-				$('div.black_block').stop().fadeIn();
-				meun_flag = true;
-				var tts = setTimeout(function() {
-				    $('html,body').addClass('no_scroll');
-					meun_flag = false;
-					clearTimeout(tts);
-				}, 500);
-				$_this.addClass('on');
-			}
-		});
+		 $(".footer-nav a").click(function(){
+			$(this).addClass('cur').siblings('a').removeClass('cur');
+		})	
+		$(".menuf-l ul li").click(function(){
+			$(this).addClass('cur').siblings('li').removeClass('cur');
+		})
 
-
-
-        //顶部分类筛选
-        (function(){
+        $(function(){
             var $layer=$('.catTab .tabLayer');
             $('.catTab').on('click','li',function(){
                 var index=$(this).index(),$this=$(this)
@@ -77,9 +26,7 @@ $(function(){
             $layer.on('click','div a',function(){
                 $(this).addClass('active').siblings('a').removeClass('active');
             })
-        })();
-       
-       
-     
-     
-    
+        })
+		
+		
+
